ff058828f9c966b15e384bbc8cd7b5f2818a2fa8
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2021-01-01 21:37:27 -0500

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,36 @@
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let gameScore = PFObject(className:"GameScore")
+        gameScore["score"] = 1337
+        gameScore["playerName"] = "Sean Plott"
+        gameScore["cheatMode"] = false
+        gameScore.saveInBackground { (success, error) in
+            if (success) {
+                
+                print("Success!")
+                
+            } else {
+                
+                print("Failed")
+                
+            }
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
